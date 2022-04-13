b6e1e0128a77505df33b60938e27ea54e074f00a
Michael Vega <michaelvega8888@gmail.com>
2022-04-13 17:39:14 -0400

    Update main.js Test main.js (with Sample files) Update README.md Update .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # xml2json
+
+Converts OPML files to json. These can later be stored in a firestore database using documents. Has one function: xmlToJSON(path: String, name: String) -> JSON.
+### Usage:
+```
+xmlToJSON("./res/", "states.opml");
+```
+* **Input**: Source to opml file
+* **Output**: JSON file (in /out)
--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 const fs = require ("fs");
 const opml = require ("opml");
 
-fs.readFile ("./res/states.opml", function (err, opmltext) {
-    if (err) {
-        console.log (err.message);
-    }
-    else {
-        opml.parse (opmltext, function (err, theOutline) { //convert OPML text into a JavaScript structure
-            //console.log ("\nThe outline's title is \"" + theOutline.opml.head.title + ".\""); //see comment at top
-            //console.log ("The third state in the Great Plains is: \"" + theOutline.opml.body.subs [0].subs [1].subs [2].text + ".\"");
-            fs.writeFile ("./out/states.json", JSON.stringify (theOutline, undefined, 4), function (err) {
-                if (err) {
-                    console.log (err.message);
-                }
-                else {
-                    console.log ("states.json was saved.");
-                }
+function xmlToJSON (path, name) {
+    console.assert(path === String);
+    console.assert(name === String);
+    path = String(path + name);
+    const nameArr = name.split(".");
+    name = String(nameArr[0]);
+    fs.readFile(path, function (err, opmltext) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            opml.parse(opmltext, function (err, theOutline) { //convert OPML text into a JavaScript structure
+                const jsonPath = "./out/" + name + ".json";
+                fs.writeFile(jsonPath, JSON.stringify(theOutline, undefined, 4), function (err) {
+                    if (err) {
+                        console.log(err.message);
+                    } else {
+                        console.log("states.json was saved.");
+                    }
+                });
+                const opmlName = name + "copy.opml";
+                const opmlCopyPath = "./out/" + opmlName;
+                    fs.writeFile(opmlCopyPath, opml.stringify(theOutline), function (err) {
+                    if (err) {
+                        console.log(err.message);
+                    } else {
+                        console.log(opmlName + " was saved.");
+                    }
+                });
+                opml.visitAll(theOutline, function (node) {
+                    node.text = node.text.toUpperCase();
+                    return true; //keep visiting
+                });
+                console.log(opml.stringify(theOutline)); //view the uppercased outline in the JS console
             });
-            fs.writeFile ("./out/statescopy.opml", opml.stringify (theOutline), function (err) {
-                if (err) {
-                    console.log (err.message);
-                }
-                else {
-                    console.log ("statescopy.opml was saved.");
-                }
-            });
-            opml.visitAll (theOutline, function (node) {
-                node.text = node.text.toUpperCase ();
-                return (true); //keep visiting
-            });
-            console.log (opml.stringify (theOutline)); //view the uppercased outline in the JS console
-        });
-    }
-});
+        }
+    });
+}
+
+xmlToJSON("./res/", "states.opml");
