0276f25206ad6ec7d705e2dd08956e7a4d4718a4
Michael Vega <michaelvega8888@gmail.com>
2022-04-18 08:20:27 -0400

    Deploy API update index.html add pugs2.js

---
+++ functions/pugs2.js
@@ -0,0 +1,29 @@
+
+const request = require("request");
+const fs = require("fs");
+
+function updateClient(postData){
+    var clientServerOptions = {
+        uri: url,
+        body: postData,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    }
+    request(clientServerOptions, function (error, response) {
+        console.log(error,response.body);
+        return;
+    });
+}
+
+
+const url = "https://us-central1-fir-functions-api-a924d.cloudfunctions.net/app/";
+let path = "../res/text.xml"
+
+// read XML from a file
+const xml = fs.readFileSync(path, "utf8");
+xml.trim();
+
+console.log(xml);
+updateClient(xml);
